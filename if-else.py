fb817562a6e38d6374eb25282eceba67d17bd014
Tanuj Kumar Yadav <148637725+tanuj-26heisnbrg@users.noreply.github.com>
2024-01-08 20:44:41 +0530

    Update if-else.py

--- if-else.py
+++ if-else.py
@@ -3,16 +3,12 @@ a = int (input ("enter your age :"))
 
 if (a<12) :
   print ("you are a child")
-
 if (12<a<19) :
    print (" you are a teenager ")
-
 if (19<a<30) :
     print  (" you are a young adult ")
-
 if (30<a<50) :
    print (" you are an adult ")
-
 if (50<a<60) :
     print (" you are a senior citizen")
 else :
